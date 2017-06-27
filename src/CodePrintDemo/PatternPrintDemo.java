13c3b05f0eec9305b1a098efbb726f8ad59f55c3
MithunBurman7 <mithunburman7@gmail.com>
2017-06-27 15:48:33 +0530

    Session4 Assignment2

---
+++ src/CodePrintDemo/PatternPrintDemo.java
@@ -0,0 +1,50 @@
+package CodePrintDemo;
+
+public class PatternPrintDemo {
+
+	public static void main(String[] args) {
+				
+				int i, j, d, x = 0;
+				char k;
+				for (i = 1; i <= 5; i++) {
+					k = 'a';
+					for (j = 1, d=0; j <= 5; j++) {
+						// increasing
+						if (i < 4) {
+							if (j >= 4 - i && j <= 2 + i) {
+								System.out.print(k);
+								if (j < 3) {
+									k++;
+								} else {
+									k--;
+								}
+							} else {
+								System.out.print(" ");
+							}
+						} else {
+							
+							// decreasing					
+							if (j>i-3 && j <= 5-d) {
+								
+								System.out.print(k);
+								if (j<3) {
+									k++;
+								} else {
+									k--;
+								}
+							} else {
+								System.out.print(" ");
+								d++;
+							}
+						}
+
+					}
+
+					System.out.println();
+				}
+			}
+
+
+
+	}
+
